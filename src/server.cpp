e986651cafec1a3ba3b9319176e36c7cf2ab97dc
daniel <daniel.zydsn@gmail.com>
2020-06-30 23:59:36 +0800

    update server

--- src/server.cpp
+++ src/server.cpp
@@ -1,12 +1,17 @@
-#include <serverConfig.h>
-#include <boost/asio.hpp>
-#include <iostream>
-#include <logSys.h>
+#include "server.h"
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 
-using namespace std;
-using boost::asio::ip::tcp;
+using sock_ptr = boost::shared_ptr<ip::tcp::socket>;
 
-void handler(const boost::system::error_code& error)
+Server::Server(string ip, int port):m_endpoint(tcp::v4(), port), m_acceptor(m_service, m_endpoint), m_socket(m_service)
+{
+    boost::system::error_code error;
+    sock_ptr sock(new ip::tcp::socket(m_service));
+    m_acceptor.async_accept(m_socket, boost::bind(&Server::accept_handler, this, error, sock));
+}
+
+void Server::accept_handler(const boost::system::error_code &error, tcp::socket socket)
 {
     if (!error)
     {
@@ -14,26 +19,7 @@ void handler(const boost::system::error_code& error)
     }
 }
 
-int main(int argc, char *argv[])
+void Server::run()
 {
-    // 初始化日志
-    initSysLog();
-    LOG(INFO, "*****************server init*****************");
-    
-    // 初始化全局配置
-    GlobalConfig& gc = GlobalConfig::instance();
-    gc.initConfig("./config/config");
-
-    // 配置服务器
-    boost::asio::io_service io_service; // 主服务
-    tcp::endpoint endpoint(tcp::v4(), gc.getPort()); //配置监听端口
-    tcp::acceptor acceptor(io_service, endpoint); // 端口监听器
-    tcp::socket skt(io_service);
-    acceptor.async_accept(skt, handler);
-
-    // 服务运行
-    LOG(INFO, "*****************server finish*****************");
-    io_service.run();
-
-    return 0;
+    m_service.run();
 }
